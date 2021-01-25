24ef794b4be68790c2743ec091aa237ba2ab6df3
Caleb Martin <caleb637@icloud.com>
2021-01-25 07:25:20 -0500

    fixed responsive gallery bug

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -4,6 +4,14 @@ const fetchImgs = async () => {
   const data = await response.json()
   return data
 }
+
+//clears all the columns before filling them to prevent duplicated images
+const clearCols = (parent) => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild)
+  }
+}
+
 //maps images into gallery
 const mapImgs = async () => {
   //fetches images from database
@@ -14,9 +22,19 @@ const mapImgs = async () => {
   const gridCol2 = document.querySelector('.grid-col--2')
   const gridCol3 = document.querySelector('.grid-col--3')
 
+  //clear cols of previous images
+  clearCols(gridCol1)
+  clearCols(gridCol2)
+  clearCols(gridCol3)
+
   // splits images into 3 grid columns
   let count = 1
+  const width = window.innerWidth > 0 ? window.innerWidth : screen.width
+
   for (let i = 0; i < images.length; i++) {
+    if (width < 600) {
+      count = 1
+    }
     // new div and img
     const newImgDiv = document.createElement('div')
     const newImg = document.createElement('img')
@@ -40,4 +58,8 @@ const mapImgs = async () => {
   }
 }
 
+//runs mapImgs on page load
 mapImgs()
+
+//reruns mapImgs on window resize
+window.addEventListener('resize', mapImgs)

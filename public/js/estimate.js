318cf139af9033f2dd0106413f7e510c2b6fde27
Caleb Martin <cmartin@moderncaliber.com>
2022-02-24 14:30:38 -0500

    fixed forward slash bug

--- public/js/estimate.js
+++ public/js/estimate.js
@@ -2,35 +2,35 @@ const galleryTitle = document.getElementById('galleryTitle')
 const formResponse = document.getElementById('formResponse')
 const formSubmit = document.querySelector('button')
 
-const name = document.getElementById('name')
-const email = document.getElementById('email')
-const phone = document.getElementById('phone')
-const address = document.getElementById('address')
-const city = document.getElementById('city')
-const state = document.getElementById('state')
-const zip = document.getElementById('zip')
-const message = document.getElementById('message')
-const spamMsg = document.getElementById('spamMsg')
+const name = document.getElementById('name').textContent
+const email = document.getElementById('email').textContent
+const phone = document.getElementById('phone').textContent
+const address = document.getElementById('address').textContent
+const city = document.getElementById('city').textContent
+const state = document.getElementById('state').textContent
+const zip = document.getElementById('zip').textContent
+const message = document.getElementById('message').textContent
+const spamMsg = document.getElementById('spamMsg').textContent
 const inputs = [name, email, phone, address, city, state, zip, message]
 
 formSubmit.addEventListener('click', (e) => {
-    e.preventDefault()
-    galleryTitle.scrollIntoView({ behavior: 'smooth' })
-    if(spamMsg.value === '') {
-        formResponse.textContent = 'Sending...'
-        fetch(`/email?name=${name.value}&email=${email.value}&phone=${phone.value}&address=${address.value}&city=${city.value}&state=${state.value}&zip=${zip.value}&message=${message.value}`)
-        .then((res) => {
-            res.json().then((data) => {
-                if(data.formResponse) {
-                    formResponse.textContent = data.formResponse
-                } else {
-                    formResponse.textContent = 'Message Sent'
-                    inputs.forEach(input => {
-                        input.value = ''
-                    })
-                }
-            })
-        })
-    }
-    
+  e.preventDefault()
+  galleryTitle.scrollIntoView({ behavior: 'smooth' })
+  if (spamMsg.value === '') {
+    formResponse.textContent = 'Sending...'
+    fetch(
+      `/email?name=${name}&email=${email}&phone=${phone}&address=${address}&city=${city}&state=${state}&zip=${zip}&message=${message}`
+    ).then((res) => {
+      res.json().then((data) => {
+        if (data.formResponse) {
+          formResponse.textContent = data.formResponse
+        } else {
+          formResponse.textContent = 'Message Sent'
+          inputs.forEach((input) => {
+            input.value = ''
+          })
+        }
+      })
+    })
+  }
 })

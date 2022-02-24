b2019597fb72ae2e4d80dea59efb6224af37a9f0
Caleb Martin <cmartin@moderncaliber.com>
2022-02-24 13:27:37 -0500

    added truegrid and fixed bugs

--- mail.js
+++ mail.js
@@ -2,27 +2,44 @@ const nodemailer = require('nodemailer')
 const mailGun = require('nodemailer-mailgun-transport')
 const validator = require('validator')
 
-
 const auth = {
-    auth: {
-        api_key: process.env.MAILGUN_API_KEY,
-        domain: process.env.MAILGUN_API_DOMAIN
-    }
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_API_DOMAIN,
+  },
 }
 
 const transporter = nodemailer.createTransport(mailGun(auth))
 
-const mailTo = (name, email, phone, address, city, state, zip, message, callback) => {
-    //validation
-    if(!name || !email || !phone || !address || !city || !state || !zip || !message) {
-        return callback('Please fill in all fields', undefined)
-    }else if(!validator.isEmail(email)) {
-        return callback('Provide a valid email', undefined)
-    } else if(!validator.isMobilePhone(phone)) {
-        return callback('Provide a valid phone number', undefined)
-    }else {
-        const output = 
-            `
+const mailTo = (
+  name,
+  email,
+  phone,
+  address,
+  city,
+  state,
+  zip,
+  message,
+  callback
+) => {
+  //validation
+  if (
+    !name ||
+    !email ||
+    !phone ||
+    !address ||
+    !city ||
+    !state ||
+    !zip ||
+    !message
+  ) {
+    return callback('Please fill in all fields', undefined)
+  } else if (!validator.isEmail(email)) {
+    return callback('Provide a valid email', undefined)
+  } else if (!validator.isMobilePhone(phone)) {
+    return callback('Provide a valid phone number', undefined)
+  } else {
+    const output = `
                 <h3>Name:</h3> ${name}
                 <h3>Phone:</h3> ${phone}
                 <h3>Email:</h3> ${email}
@@ -32,21 +49,21 @@ const mailTo = (name, email, phone, address, city, state, zip, message, callback
                 <h3>Zipcode:</h3> ${zip}
                 <h3>Message:</h3> ${message}
             `
-        const mailOptions = {
-            from: email,
-            to: 'jesse@redoakinc.org',
-            subject: 'Red Oak Estimate',
-            html: output
-        }
-        
-        transporter.sendMail(mailOptions, (err, data) => {
-            if(err) {
-                callback('Internal Error', undefined)
-            } else {
-                callback(undefined, data)
-            }
-        })
+    const mailOptions = {
+      from: email,
+      to: 'jesse@redoakinc.org',
+      subject: 'Red Oak Estimate',
+      html: output,
     }
+
+    transporter.sendMail(mailOptions, (err, data) => {
+      if (err) {
+        callback('Internal Error', undefined)
+      } else {
+        callback(undefined, data)
+      }
+    })
+  }
 }
 
 module.exports = mailTo
